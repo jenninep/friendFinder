a7971e5d16429820dc9ee4e8d3955a934d18bd91
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2016-07-31 11:31:02 -0400

    initial commit

---
+++ routing/html_routes.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'home.html'))
+});
+
+app.listen(PORT, function(){
+	console.log("App is listening on port:" + PORT);
+});
